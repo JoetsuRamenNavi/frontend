5420a044890acd0227c70fd8131205522b0a1a04
ryuichi <ryuichi19633811@gmail.com>
2023-06-02 10:28:54 +0900

    add: z-index追加

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header: NextPage = () => {
   return (
-    <div className="h-[65px] w-full fixed top-0 left-0 pl-[10px] bg-white flex items-center baseShadow">
+    <div className="h-[65px] w-full fixed top-0 left-0 pl-[10px] bg-white flex items-center baseShadow z-10">
       <Link href="/">
         <p className="text-[24px] font-semibold">上越ラーメンアプリ</p>
       </Link>
