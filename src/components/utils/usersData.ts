7222e55de442a8276b782ac1fa07762adef95d80
ryuichi <ryuichi19633811@gmail.com>
2023-06-13 13:04:58 +0900

    add: モックデータ

---
+++ src/components/utils/usersData.ts
@@ -0,0 +1,42 @@
+export const users = [
+  {
+    id: 0,
+    name: "Ryuichi Fujitia",
+    password: "pass",
+    age: "12",
+    battle: {
+      date: ["5/31", "6/1", "6/7"],
+      flg: [true, true, false],
+      store: ["手打ちらーめん まるとく", "春紀", "春紀"],
+      name: [
+        "酒かす鶏ポタージュ",
+        "酒かすラーメン 白味噌仕立て",
+        "酒かすラーメン 白味噌仕立て",
+      ],
+    },
+  },
+  {
+    id: 1,
+    name: "Takuya Fujiwara",
+    password: "pass",
+    age: "13",
+    battle: {
+      date: ["5/31", "6/7"],
+      flg: [true, true, false],
+      store: ["手打ちらーめん まるとく", "春紀"],
+      name: ["酒かす鶏ポタージュ", "酒かすラーメン 白味噌仕立て"],
+    },
+  },
+  {
+    id: 2,
+    name: "Yume Funaki",
+    password: "pass",
+    age: "12",
+    battle: {
+      date: ["6/7"],
+      flg: [false],
+      store: ["手打ちらーめん まるとく"],
+      name: ["酒かす鶏ポタージュ"],
+    },
+  },
+];
