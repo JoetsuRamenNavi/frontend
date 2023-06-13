7139d9fee7b138c870ab6c900991d912708edbfd
ryuichi <ryuichi19633811@gmail.com>
2023-06-13 13:05:40 +0900

    create: ページ

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layouts/Layout";
+import { NextPage } from "next";
+
+const Signup: NextPage = () => {
+  return (
+    <Layout>
+      <div>Enter</div>
+    </Layout>
+  );
+};
+
+export default Signup;
