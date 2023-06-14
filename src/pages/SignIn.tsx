22262006eafd2898e587643f6ad1ca2e2629896a
FX31 <xg92xg@gmail.com>
2023-06-14 17:10:53 +0900

    login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,64 @@
-import Layout from "@/components/layouts/Layout";
-import { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { users } from "@/components/utils/usersData";
 
-const SignIn: NextPage = () => {
+type Inputs = {
+  id: string;
+  password: string;
+};
+
+// usersDataの型
+type User = {
+  id: number;
+  name: string;
+  password: string;
+  age: number;
+  battle: {
+    date: string[];
+    flg: boolean[];
+    store: string[];
+    name: string[];
+  };
+};
+
+const SignIn: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const [loginUser, setLoginUser] = useState<User | null>(null);
+
+  const onSubmit = ({ id, password }: Inputs) => {
+    const user = users.find(
+      (user) => user.id === Number(id) && user.password === password
+    );
+    if (user) {
+      setLoginUser(user);
+    } else {
+      alert("IDかパスワードが間違っています");
+    }
+  };
+
   return (
-    <Layout>
-      <div>Enter</div>
-    </Layout>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="number"
+        placeholder="ID"
+        {...register("id", { required: true })}
+      />
+      {errors.id && <p>This field is required</p>}
+
+      <input
+        type="password"
+        placeholder="Password"
+        {...register("password", { required: true })}
+      />
+      {errors.password && <p>This field is required</p>}
+
+      <input type="submit" />
+      {loginUser && <p>ログイン成功: {loginUser.name}</p>}
+    </form>
   );
 };
 
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,12 +1,55 @@
-import Layout from "@/components/layouts/Layout";
-import { NextPage } from "next";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { users } from "@/components/utils/usersData";
 
-const Signup: NextPage = () => {
+type NewUser = {
+  id: string;
+  password: string;
+};
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewUser>();
+
+  const onSubmit: SubmitHandler<NewUser> = (data) => {
+    const existingUser = users.find((user) => user.id === Number(data.id));
+    if (existingUser) {
+      alert("このIDはすでに使われています。");
+      return;
+    }
+    const newUser = {
+      id: Number(data.id),
+      password: data.password,
+      name: "",
+      age: 0,
+      battle: {
+        date: [],
+        flg: [],
+        store: [],
+        name: [],
+      },
+    };
+    users.push(newUser);
+    alert("登録が完了しました。");
+  };
+
   return (
-    <Layout>
-      <div>Enter</div>
-    </Layout>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        ID:
+        <input type="text" {...register("id", { required: true })} />
+        {errors.id && <span>This field is required</span>}
+      </label>
+      <label>
+        Password:
+        <input type="password" {...register("password", { required: true })} />
+        {errors.password && <span>This field is required</span>}
+      </label>
+      <input type="submit" />
+    </form>
   );
 };
 
-export default Signup;
+export default SignUp;
